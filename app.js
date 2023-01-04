79ae6077eb1ec44827031c61ad09e61be0ba819b
nomanarshad1352001 <113524745+nomanarshad1352001@users.noreply.github.com>
2023-01-04 12:55:43 +0500

    Template Engines

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser');
 const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const rootDir = require('./Utils/path')
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended:false}));
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next)=> {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-app.listen(3000)
+app.listen(3000);
